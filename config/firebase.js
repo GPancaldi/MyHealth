c1ed19be63363f8ba80b37b17290c3058a957dac
GPancaldi <pancaldi@alunos.utfpr.edu.br>
2022-11-23 19:56:43 -0300

    firestore

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,8 @@
 
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
  import {getAuth} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+ import { initializeFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+ import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
  
 
   
@@ -8,16 +10,22 @@
   const firebaseConfig = {
     apiKey: "AIzaSyBy0PNH1YGGRJfmANCpnpJy98VTd-93n80",
     authDomain: "myhealthweb-eca16.firebaseapp.com",
+    databaseURL: "https://myhealthweb-eca16-default-rtdb.firebaseio.com",
     projectId: "myhealthweb-eca16",
     storageBucket: "myhealthweb-eca16.appspot.com",
     messagingSenderId: "769703408786",
-    appId: "1:769703408786:web:61d7f8e5e51e6d4f67624b"
+    appId: "1:769703408786:web:0fb02c82a89858eb67624b"
   };
 
   const app = initializeApp(firebaseConfig);
+  
   const auth = getAuth(app)
 
-export {app, auth}
+  const db = initializeFirestore(app, {experimentalForceLongPolling: true})
+
+  const storage = getStorage(app)
+
+export {app, auth, db, storage}
  
 
   
